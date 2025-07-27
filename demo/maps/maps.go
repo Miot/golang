e2e37accc4f1958eea379d0d67f87a1b2985cf12
Miot <imuy@foxmail.com>
2025-07-27 23:31:34 +0800

    map

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 10
+	shoppingList["bread"] += 1
+	shoppingList["eggs"] += 1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	fmt.Println("Need cereal?")
+	if found {
+		fmt.Println("Yes", cereal)
+	} else {
+		fmt.Println("No")
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("Total items:", totalItems)
 }
 
