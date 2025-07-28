62d711af511699b617b8ff9a49f7a19015b4d6d9
Miot <imuy@foxmail.com>
2025-07-28 23:59:05 +0800

    variadics

--- demo/variadics/variadics.go
+++ demo/variadics/variadics.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
 
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+	all := append(a, b...)
+	fmt.Println(sum(all...))
 }
 
