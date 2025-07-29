5b00b892fc63bb655a146dd57ef3ff211e29dfd1
Miot <imuy@foxmail.com>
2025-07-29 18:31:51 +0800

    interface

--- demo/interfaces/interfaces.go
+++ demo/interfaces/interfaces.go
@@ -2,7 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("cook salad")
+	fmt.Println("add dressing")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("prepare dishes:")
+	for i := range(dishes) {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
+func main() {
+	dishes := []Preparer{
+		Chicken("Chicken Wings"),
+		Salad("Iceberg Salad"),
+	}
+
+	prepareDishes(dishes)
 }
 
--- exercise/interfaces/interfaces.go
+++ exercise/interfaces/interfaces.go
@@ -21,7 +21,63 @@ package main
 
 import "fmt"
 
-func main() {
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
 
+type Lift int
+type LiftPicker interface {
+	PickLift() Lift
+}
+
+type Motorcycles string
+type Car string
+type Truck string
+
+func (m Motorcycles) String() string {
+	return fmt.Sprintf("Motorcycles: %v", string(m))
+}
+
+func (m Motorcycles) PickLift() Lift {
+	return SmallLift
+}
+
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v", string(c))
+}
+
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(t))
+}
+
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+func sendToLift(p LiftPicker) {
+	switch p.PickLift() {
+	case SmallLift:
+		fmt.Printf("send %v to small lift\n", p)
+	case StandardLift:
+		fmt.Printf("send %v to standard lift\n", p)
+	case LargeLift:
+		fmt.Printf("send %v to large lift\n", p)
+	}
+}
+
+func main() {
+	car := Car("Sporty")
+	truck := Truck("MountainCrusher")
+	motorcycles := Motorcycles("StreetRacer")
+
+	sendToLift(car)
+	sendToLift(truck)
+	sendToLift(motorcycles)
 }
 
