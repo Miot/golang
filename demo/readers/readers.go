7e8448b39decd232badc4fae03e367d1d265c078
Miot <imuy@foxmail.com>
2025-07-29 22:35:22 +0800

    reader

--- demo/readers/readers.go
+++ demo/readers/readers.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"fmt";
+	"bufio";
+	"strings";
+	"strconv";
+	"io";
+	"os"
 )
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	sum := 0
+	for {
+		input, inputErr := r.ReadString(' ')
+		n := strings.TrimSpace(input)
+		if n == "" {
+			continue
+		}
+		num, convErr := strconv.Atoi(n)
+		if convErr != nil {
+			fmt.Println("Conversion error:", convErr)
+		} else {
+			sum += num
+		}
+
+		if inputErr == io.EOF {
+			break
+		}
+		if inputErr != nil {
+			fmt.Println("Error reading Stdin:", inputErr)
+		}
+	}
+
+	fmt.Printf("Sum: %v\n", sum)
 }
 
--- exercise/reader/reader.go
+++ exercise/reader/reader.go
@@ -18,9 +18,40 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	CmdHello = "hello"
+	CmdBye   = "bye"
+)
 
 func main() {
-
+	scanner := bufio.NewScanner(os.Stdin)
+	numLines := 0
+	numCommands := 0
+	for scanner.Scan() {
+		if strings.ToUpper(scanner.Text()) == "Q" {
+			break
+		} else {
+			text := strings.TrimSpace(scanner.Text())
+			switch text {
+			case CmdHello:
+				numCommands += 1
+				fmt.Println("Hi!!!!")
+			case CmdBye:
+				numCommands += 1
+				fmt.Println("Bye!!!")
+			}
+			if text != "" {
+				numLines += 1
+			}
+		}
+	}
+	fmt.Printf("You entered %v lines and %v commands\n", numLines, numCommands)
 }
 
