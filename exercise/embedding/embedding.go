5565887bb05a45cec74fa46c6c05b74ce8e14e04
Miot <imuy@foxmail.com>
2025-07-30 18:24:07 +0800

    embedding

--- exercise/embedding/embedding.go
+++ exercise/embedding/embedding.go
@@ -31,9 +31,43 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (b *BandwidthUsage) AverageBandwidth() int {
+	sum := 0
+	for _, value := range b.amount {
+		sum += int(value)
+	}
+	return sum / len(b.amount)
+}
+
+func (c *CpuTemp) AverageTemp() int {
+	sum := 0
+	for _, value := range c.temp {
+		sum += int(value)
+	}
+	return sum / len(c.temp)
+}
+
+func (m *MemoryUsage) AverageMemory() int {
+	sum := 0
+	for _, value := range m.amount {
+		sum += int(value)
+	}
+	return sum / len(m.amount)
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+	dashboard := Dashboard{bandwidth, temp, memory}
+	fmt.Println(dashboard.AverageBandwidth())
+	fmt.Println(dashboard.AverageTemp())
+	fmt.Println(dashboard.AverageMemory())
 }
 
