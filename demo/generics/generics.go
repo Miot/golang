d7321295201ddb3e650d270f4e544784605db1fb
Miot <imuy@foxmail.com>
2025-07-30 20:52:51 +0800

    generics

--- demo/generics/generics.go
+++ demo/generics/generics.go
@@ -8,6 +8,47 @@ const (
 	High
 )
 
+type PrriorityQueue[P comparable, V any] struct {
+	items map[P][]V
+	priorities []P
+}
+
+func NewPriorityQueue[P comparable, V any](priorities []P) PrriorityQueue[P, V] {
+	return PrriorityQueue[P, V]{
+		items: make(map[P][]V),
+		priorities: priorities,
+	}
+}
+
+func (pq *PrriorityQueue[P, V]) Add(priority P, value V) {
+	pq.items[priority] = append(pq.items[priority], value)
+}
+
+func (pq *PrriorityQueue[P, V]) Next() (V, bool) {
+	for _, priority := range pq.priorities {
+		if len(pq.items[priority]) > 0 {
+			next := pq.items[priority][0]
+			pq.items[priority] = pq.items[priority][1:]
+			return next, true
+		}
+	}
+	var empty V
+	return empty, false
+}
+
 func main() {
+	pq := NewPriorityQueue[int, string]([]int{High, Medium, Low})
+	pq.Add(Low, "Low 1")
+	pq.Add(High, "High 1")
+	fmt.Println(pq.Next())
+
+	pq.Add(Medium, "Medium 1")
+	pq.Add(High, "High 2")
+	pq.Add(High, "High 3")
+	fmt.Println(pq.Next())
+	fmt.Println(pq.Next())
+	fmt.Println(pq.Next())
+	fmt.Println(pq.Next())
+	fmt.Println(pq.Next())
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module coursecontent
 
-go 1.21
+go 1.23.0
+
+toolchain go1.24.5
+
+require (
+	github.com/chai2010/webp v1.4.0
+	github.com/google/uuid v1.6.0
+	golang.org/x/exp v0.0.0-20250718183923-645b1fa84792
+)
